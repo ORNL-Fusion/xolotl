4c64253b3794af6b8f914860634e3f0cfb24d4cc
jayjaybillings <jayjaybillings@mycompany.com>
2012-10-03 19:51:19 +0000

    Committing "Hello World!" CPP code and start of CMake build system. ~JJB 20121003 15:51
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/trunk@5 72105f29-0fe1-43bd-9363-b1e281e962a2

---
+++ xolotl/src/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * Main.c, currently just hello world
+ */
+#include<cstdlib>
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+	cout << "Hello World!" << endl;
+
+	return EXIT_SUCCESS;
+}
