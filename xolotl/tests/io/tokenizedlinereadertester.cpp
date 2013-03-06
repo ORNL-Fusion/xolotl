229dc4a4c5d8be59b304bb5703f9d92fa6f415b0
jayjaybillings <jayjaybillings@mycompany.com>
2013-03-06 03:21:02 +0000

    Fixed erroneous documentation and continued test and production source implementation for parser. ~JJB 20130305 22:22
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/trunk@21 72105f29-0fe1-43bd-9363-b1e281e962a2

--- xolotl/tests/io/tokenizedlinereadertester.cpp
+++ xolotl/tests/io/tokenizedlinereadertester.cpp
@@ -13,24 +13,58 @@ BOOST_AUTO_TEST_SUITE(TokenizedLineReader_testSuite)
 BOOST_AUTO_TEST_CASE(checkDefaultParsing) {
 
 	// Local Declarations
-	string doubleString = "0.0 1.0 5.0\r\n0.11 0.55 99.283\r\n38.2 883.33 74.832\r\n";
+	string doubleString = "0.0 1.0 5.0\r\n0.11 0.55 22.86 99.283\r\n0.000382 883.33 74.832\r\n";
 	TokenizedLineReader<double> doubleReader;
 	TokenizedLineReader<int> intReader;
 	TokenizedLineReader<bool> boolReader;
+	vector<double> dLine;
 
-	// Try parsing the doubles
-	shared_ptr<istream> testStream(new stringstream(stringstream::in | stringstream::out));
+	//----- Check doubles -----//
+
+	// Create the input stream
+	shared_ptr<stringstream> testStream(new stringstream(stringstream::in | stringstream::out));
+	*(testStream.get()) << doubleString;
+	// Load the double reader
+	doubleReader.setInputStream(testStream);
+	// Get the first line and check it
+	dLine = doubleReader.loadLine();
+	BOOST_REQUIRE(!dLine.empty());
+	BOOST_REQUIRE_EQUAL(3,dLine.size());
+	BOOST_REQUIRE_CLOSE_FRACTION(0.0,dLine.at(0),0.001);
+	BOOST_REQUIRE_CLOSE_FRACTION(1.0,dLine.at(1),0.001);
+	BOOST_REQUIRE_CLOSE_FRACTION(5.0,dLine.at(2),0.001);
+	// Get the second line and check it
+	dLine = doubleReader.loadLine();
+	BOOST_REQUIRE(!dLine.empty());
+	BOOST_REQUIRE_EQUAL(4,dLine.size());
+	BOOST_REQUIRE_CLOSE_FRACTION(0.11,dLine.at(0),0.001);
+	BOOST_REQUIRE_CLOSE_FRACTION(0.55,dLine.at(1),0.001);
+	BOOST_REQUIRE_CLOSE_FRACTION(22.86,dLine.at(2),0.001);
+	BOOST_REQUIRE_CLOSE_FRACTION(99.283,dLine.at(3),0.0001);
+	// Get the third line and check it
+	dLine = doubleReader.loadLine();
+	BOOST_REQUIRE(!dLine.empty());
+	BOOST_REQUIRE_EQUAL(3,dLine.size());
+	BOOST_REQUIRE_CLOSE_FRACTION(0.000382,dLine.at(0),0.001);
+	BOOST_REQUIRE_CLOSE_FRACTION(883.33,dLine.at(1),0.001);
+	BOOST_REQUIRE_CLOSE_FRACTION(74.382,dLine.at(2),0.0001);
 
 	return;
 }
 
 /**This operation checks the TokenizedLineReader when the token is changed to a comma.*/
 BOOST_AUTO_TEST_CASE(checkCSVParsing) {
+
+	BOOST_FAIL( "Test not yet implemented" );
+
 	return;
 }
 
 /**This operation checks the TokenizedLineReader when the comment token is changed.*/
 BOOST_AUTO_TEST_CASE(checkCommentTokens) {
+
+	BOOST_FAIL( "Test not yet implemented" );
+
 	return;
 }
 
--- xolotl/xolotlCore/io/tokenizedlinereader.h
+++ xolotl/xolotlCore/io/tokenizedlinereader.h
@@ -1,20 +1,18 @@
 #ifndef TOKENIZEDLINEREADER_H
 #define TOKENIZEDLINEREADER_H
-//Begin section for file tokenizedlinereader.h
-//End section for file tokenizedlinereader.h
 
 namespace xolotlCore {
 
 /**
  * This is a simple class for parsing lines from an input stream and
- * splitting that line based on a token. A line in this case is defined as any
+ * splitting that line to retrieve delimiters. A line in this case is defined as any
  * sequence of characters that terminates with an end-of-line character such
- * as \r, \n or \r\n. The default token is a single space and the token can be
- * changed by calling the setToken() operation. Lines that are meant to be
+ * as \r, \n or \r\n. The default delimiter is a single space and the delimiter can be
+ * changed by calling the setDelimiter() operation. Lines that are meant to be
  * comments will be skipped and this includes lines that start with a "#" by
- * default. The comment token can be changed by calling setCommentToken().
+ * default. The comment delimiter can be changed by calling setCommentDelimiter().
  * "In-line comments" where the comment starts at some later point in the line
- * are not allowed and will be parsed as data.</p><p>This class is templated so
+ * are not allowed and will be parsed as data. This class is templated so
  * that conversion to primitive types can be handled automatically for the
  * client class. This class is only meant to be templated with primitive types
  * and it will fail for classes or interfaces.
@@ -24,11 +22,11 @@ class TokenizedLineReader {
 
 private:
 
-	//!The delimiter or "token" that separates elements in the line.
-	const std::string dataToken;
+	//!The delimiter or "delimiter" that separates elements in the line.
+	const std::string dataDelimiter;
 
 	//!The character that represents a line that is a comment and should not be parsed.
-	const std::string commentToken;
+	const std::string commentDelimiter;
 
 	//!The inputstream from which data should be read.
 	std::shared_ptr<std::istream> inputstream;
@@ -37,22 +35,23 @@ public:
 
 	//!The constructor
 	TokenizedLineReader() :
-		dataToken(" "), commentToken("#") {}
+		dataDelimiter(" "), commentDelimiter("#") {
+	}
 
 	/**
-	 * This operation sets the string (token) that represents the delimiter separating the different data elements in the line.
+	 * This operation sets the string (delimiter) that represents the delimiter separating the different data elements in the line.
 	 *
 	 */
-	void setToken(const std::string token) {
+	void setDelimiter(const std::string delimiter) {
 		//TODO Auto-generated method stub
 		return;
 	}
 
 	/**
-	 * This operation sets the character (token) that represents the comment character.
+	 * This operation sets the character (delimiter) that represents the comment character.
 	 *
 	 */
-	void setCommentToken(const std::string cToken) {
+	void setCommentDelimiter(const std::string cdelimiter) {
 		//TODO Auto-generated method stub
 		return;
 	}
@@ -67,11 +66,23 @@ public:
 
 	/**
 	 * This operation loads the next line from the stream and returns an array
-	 * of its elements by splitting on the token or null if there are no more
+	 * of its elements by splitting on the delimiter or null if there are no more
 	 * lines in the stream.
 	 */
 	std::vector<dataType> loadLine() {
-		//TODO Auto-generated method stub
+
+		// Local Declarations
+		std::vector<dataType> lineVector;
+		std::string line;
+
+		// Check the inputstream before reading from it
+		if (!inputstream->eof() && inputstream->good()) {
+			// Get the line
+			std::getline(*(inputstream.get()), line);
+			// Split it
+		}
+
+		return lineVector;
 	}
 
 }; //end class TokenizedLineReader
