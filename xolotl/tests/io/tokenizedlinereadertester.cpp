9c2d63c7d046935cb7c6808fddcf42b1169a36df
jayjaybillings <jayjaybillings@mycompany.com>
2013-03-15 20:30:49 +0000

    Finished TokenizedLineReader and tests. ~JJB 20130315 16:30
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/trunk@24 72105f29-0fe1-43bd-9363-b1e281e962a2

--- xolotl/tests/io/tokenizedlinereadertester.cpp
+++ xolotl/tests/io/tokenizedlinereadertester.cpp
@@ -7,26 +7,33 @@
 using namespace std;
 using namespace xolotlCore;
 
+// Test data
+const string doubleString =
+		"0.0 1.0 5.0\n0.11 0.55 22.86 99.283\n# Comment\n0.000382 883.33 74.832\n";
+const string doubleCSVDoubleSlashCommentString =
+		"0.0,1.0,5.0\n0.11,0.55,22.86,99.283\n// Comment\n0.000382,883.33,74.832\n";
+const string intString = "1 3 5 7 9\n# Comment\n 0 2 4 6 \n";
+const string intCSVString = "1,3,5,7,9\n# Comment\n0,2,4,6\n";
+
 BOOST_AUTO_TEST_SUITE( TokenizedLineReader_testSuite)
 
 /**This operation checks default parsing setup of the TokenizedLineReader.*/
 BOOST_AUTO_TEST_CASE(checkDefaultParsing) {
 
 	// Local Declarations
-	string doubleString = "0.0 1.0 5.0\r\n0.11 0.55 22.86 99.283\r\n# Comment\r\n0.000382 883.33 74.832\r\n";
 	TokenizedLineReader<double> doubleReader;
 	TokenizedLineReader<int> intReader;
-	TokenizedLineReader<bool> boolReader;
 	vector<double> dLine;
+	vector<int> iLine;
 
 	//----- Check doubles -----//
 
 	// Create the input stream
-	shared_ptr<stringstream> testStream(
+	shared_ptr<stringstream> doubleTestStream(
 			new stringstream(stringstream::in | stringstream::out));
-	*(testStream.get()) << doubleString;
+	*doubleTestStream << doubleString;
 	// Load the double reader
-	doubleReader.setInputStream(testStream);
+	doubleReader.setInputStream(doubleTestStream);
 	// Get the first line and check it
 	dLine = doubleReader.loadLine();
 	BOOST_REQUIRE(!dLine.empty());
@@ -46,13 +53,37 @@ BOOST_AUTO_TEST_CASE(checkDefaultParsing) {
 	// line and check it
 	dLine = doubleReader.loadLine();
 	BOOST_REQUIRE(!dLine.empty());
-	for(int i = 0; i < dLine.size(); i++) {
-		std::cout << dLine.at(i) << std::endl;
-	}
 	BOOST_REQUIRE_EQUAL(3,dLine.size());
 	BOOST_REQUIRE_CLOSE_FRACTION(0.000382,dLine.at(0),0.001);
 	BOOST_REQUIRE_CLOSE_FRACTION(883.33,dLine.at(1),0.001);
 	BOOST_REQUIRE_CLOSE_FRACTION(74.832,dLine.at(2),0.0001);
+
+	//----- Check integers -----//
+
+	// Create the input stream
+	shared_ptr<stringstream> intTestStream(
+			new stringstream(stringstream::in | stringstream::out));
+	*intTestStream << intString;
+	// Load the int reader
+	intReader.setInputStream(intTestStream);
+	// Get the first line and check it
+	iLine = intReader.loadLine();
+	BOOST_REQUIRE(!iLine.empty());
+	BOOST_REQUIRE_EQUAL(5,iLine.size());
+	BOOST_REQUIRE_EQUAL(1,iLine.at(0));
+	BOOST_REQUIRE_EQUAL(3,iLine.at(1));
+	BOOST_REQUIRE_EQUAL(5,iLine.at(2));
+	BOOST_REQUIRE_EQUAL(7,iLine.at(3));
+	BOOST_REQUIRE_EQUAL(9,iLine.at(4));
+	// The second line should skipped because it is a comment so, get the fourth
+	// line and check it
+	iLine = intReader.loadLine();
+	BOOST_REQUIRE(!iLine.empty());
+	BOOST_REQUIRE_EQUAL(4,iLine.size());
+	BOOST_REQUIRE_EQUAL(0,iLine.at(0));
+	BOOST_REQUIRE_EQUAL(2,iLine.at(1));
+	BOOST_REQUIRE_EQUAL(4,iLine.at(2));
+	BOOST_REQUIRE_EQUAL(6,iLine.at(3));
 
 	return;
 }
@@ -60,15 +91,84 @@ BOOST_AUTO_TEST_CASE(checkDefaultParsing) {
 /**This operation checks the TokenizedLineReader when the token is changed to a comma.*/
 BOOST_AUTO_TEST_CASE(checkCSVParsing) {
 
-	BOOST_FAIL( "Test not yet implemented" );
+	// Local Declarations
+	TokenizedLineReader<int> intReader;
+	vector<int> iLine;
+
+	//----- Check integers -----//
+
+	// Create the input stream
+	shared_ptr<stringstream> intTestStream(
+			new stringstream(stringstream::in | stringstream::out));
+	*intTestStream << intCSVString;
+	// Configure the delimiter
+	intReader.setDelimiter(",");
+	// Load the int reader
+	intReader.setInputStream(intTestStream);
+	// Get the first line and check it
+	iLine = intReader.loadLine();
+	BOOST_REQUIRE(!iLine.empty());
+	BOOST_REQUIRE_EQUAL(5,iLine.size());
+	BOOST_REQUIRE_EQUAL(1,iLine.at(0));
+	BOOST_REQUIRE_EQUAL(3,iLine.at(1));
+	BOOST_REQUIRE_EQUAL(5,iLine.at(2));
+	BOOST_REQUIRE_EQUAL(7,iLine.at(3));
+	BOOST_REQUIRE_EQUAL(9,iLine.at(4));
+	// The second line should skipped because it is a comment so, get the fourth
+	// line and check it
+	iLine = intReader.loadLine();
+	BOOST_REQUIRE(!iLine.empty());
+	BOOST_REQUIRE_EQUAL(4,iLine.size());
+	BOOST_REQUIRE_EQUAL(0,iLine.at(0));
+	BOOST_REQUIRE_EQUAL(2,iLine.at(1));
+	BOOST_REQUIRE_EQUAL(4,iLine.at(2));
+	BOOST_REQUIRE_EQUAL(6,iLine.at(3));
 
 	return;
 }
 
 /**This operation checks the TokenizedLineReader when the comment token is changed.*/
-BOOST_AUTO_TEST_CASE(checkCommentTokens) {
+BOOST_AUTO_TEST_CASE(checkCommentDelimiter) {
 
-	BOOST_FAIL( "Test not yet implemented" );
+	// Local Declarations
+	TokenizedLineReader<double> doubleReader;
+	vector<double> dLine;
+
+	//----- Check doubles -----//
+
+	// Create the input stream
+	shared_ptr<stringstream> doubleTestStream(
+			new stringstream(stringstream::in | stringstream::out));
+	*doubleTestStream << doubleCSVDoubleSlashCommentString;
+	// Set the delimiter
+	doubleReader.setDelimiter(",");
+	// Set the comment delimiter
+	doubleReader.setCommentDelimiter("//");
+	// Load the double reader
+	doubleReader.setInputStream(doubleTestStream);
+	// Get the first line and check it
+	dLine = doubleReader.loadLine();
+	BOOST_REQUIRE(!dLine.empty());
+	BOOST_REQUIRE_EQUAL(3,dLine.size());
+	BOOST_REQUIRE_CLOSE_FRACTION(0.0,dLine.at(0),0.001);
+	BOOST_REQUIRE_CLOSE_FRACTION(1.0,dLine.at(1),0.001);
+	BOOST_REQUIRE_CLOSE_FRACTION(5.0,dLine.at(2),0.001);
+	// Get the second line and check it
+	dLine = doubleReader.loadLine();
+	BOOST_REQUIRE(!dLine.empty());
+	BOOST_REQUIRE_EQUAL(4,dLine.size());
+	BOOST_REQUIRE_CLOSE_FRACTION(0.11,dLine.at(0),0.001);
+	BOOST_REQUIRE_CLOSE_FRACTION(0.55,dLine.at(1),0.001);
+	BOOST_REQUIRE_CLOSE_FRACTION(22.86,dLine.at(2),0.001);
+	BOOST_REQUIRE_CLOSE_FRACTION(99.283,dLine.at(3),0.0001);
+	// The third line should skipped because it is a comment so, get the fourth
+	// line and check it
+	dLine = doubleReader.loadLine();
+	BOOST_REQUIRE(!dLine.empty());
+	BOOST_REQUIRE_EQUAL(3,dLine.size());
+	BOOST_REQUIRE_CLOSE_FRACTION(0.000382,dLine.at(0),0.001);
+	BOOST_REQUIRE_CLOSE_FRACTION(883.33,dLine.at(1),0.001);
+	BOOST_REQUIRE_CLOSE_FRACTION(74.832,dLine.at(2),0.0001);
 
 	return;
 }
--- xolotl/xolotlCore/io/tokenizedlinereader.h
+++ xolotl/xolotlCore/io/tokenizedlinereader.h
@@ -8,7 +8,8 @@ namespace xolotlCore {
  * splitting that line to retrieve delimiters. A line in this case is defined as any
  * sequence of characters that terminates with an end-of-line character such
  * as \r, \n or \r\n. The default delimiter is a single space and the delimiter can be
- * changed by calling the setDelimiter() operation. Lines that are meant to be
+ * changed by calling the setDelimiter() operation. Delimiters found at the beginning
+ * and end of the lines are removed. Lines that are meant to be
  * comments will be skipped and this includes lines that start with a "#" by
  * default. The comment delimiter can be changed by calling setCommentDelimiter().
  * "In-line comments" where the comment starts at some later point in the line
@@ -23,10 +24,10 @@ class TokenizedLineReader {
 private:
 
 	//!The delimiter or "delimiter" that separates elements in the line.
-	const std::string dataDelimiter;
+	std::string dataDelimiter;
 
 	//!The character that represents a line that is a comment and should not be parsed.
-	const std::string commentDelimiter;
+	std::string commentDelimiter;
 
 	//!The inputstream from which data should be read.
 	std::shared_ptr<std::istream> inputstream;
@@ -43,7 +44,9 @@ public:
 	 *
 	 */
 	void setDelimiter(const std::string delimiter) {
-		//TODO Auto-generated method stub
+
+		dataDelimiter = delimiter;
+
 		return;
 	}
 
@@ -52,7 +55,9 @@ public:
 	 *
 	 */
 	void setCommentDelimiter(const std::string cdelimiter) {
-		//TODO Auto-generated method stub
+
+		commentDelimiter = cdelimiter;
+
 		return;
 	}
 
@@ -82,34 +87,44 @@ public:
 		// Check the inputstream before reading from it
 		if (!inputstream->eof() && inputstream->good()) {
 			// Get the line
-			std::getline(*(inputstream.get()), line);
-			// Split it if it is not empty and does not start with the comment character
-			std::cout << line << std::endl;
+			std::getline(*inputstream, line);
+			// Split it if it is not empty and does not start with the comment
+			// character
 			if (!line.empty()) {
-				// If this line is a comment, skip it by calling this operation again
-				// FIXME! - Update to compiler to support C++11 and use std::string.front()!
+				// If this line is a comment, skip it by calling this operation
+				// again - FIXME! - Update to compiler to support C++11 and use
+				// std::string.front()!
 				if (line.find(commentDelimiter) == 0)
 					return loadLine();
-				// Only split the line if it contains the delimiter
+				//				// Remove delimiters at the beginning of the string
+				if (line.find(dataDelimiter) == 0)
+					line = line.substr(1);
+				// Remove delimiters at the end of the string
+				if (line.find(dataDelimiter, line.size() - 2) == line.size()
+						- 1)
+					line = line.erase(line.size() - 1);
+				// Find the first instance of the delimiter
 				nextDelimiterPos = line.find(dataDelimiter);
+				// Only split the line if it contains the delimiter
 				if (nextDelimiterPos != finalDelimiterPos) {
+					// Walk across each piece of data in the line stopping only
+					// when the end of the string is reached.
 					while (lastDelimiterPos != finalDelimiterPos) {
-						std::cout << "last and next delimiter positions "
-								<< lastDelimiterPos + 1 << " "
-								<< nextDelimiterPos << std::endl;
-						subLine = line.substr(lastDelimiterPos + 1,
-								nextDelimiterPos - lastDelimiterPos + 1);
-						// SUBLINE is not updating correctly
+						// Create the substring that holds the single piece of
+						// data between the delimiters
+						subLine = line.substr(lastDelimiterPos,
+								nextDelimiterPos - lastDelimiterPos);
+						// Clear the stream, reset the substring, load the data
 						dataStream.clear();
 						dataStream.str(subLine);
-						// AND dataLine is wrong
 						dataStream >> data;
-						std::cout << "subLine = " << subLine << " with data "
-								<< data << std::endl;
 						dataVector.push_back(data);
-						lastDelimiterPos = nextDelimiterPos;
+						// Switch the delimiter positions and find the next
+						lastDelimiterPos = (nextDelimiterPos
+								!= finalDelimiterPos) ? nextDelimiterPos + 1
+								: nextDelimiterPos;
 						nextDelimiterPos = line.find(dataDelimiter,
-								lastDelimiterPos + 1);
+								lastDelimiterPos);
 					}
 				} else {
 					// Otherwise just put the whole line in the array
