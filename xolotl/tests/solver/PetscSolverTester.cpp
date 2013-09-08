5a7e610912f3f939977e959afc96451d112ff57a
jayjaybillings <jayjaybillings@mycompany.com>
2013-09-08 19:10:14 +0000

    Added a new test for the PetscSolver so that I can put the conversion routines under test. ~JJB 20130908 15:10
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/trunk@150 72105f29-0fe1-43bd-9363-b1e281e962a2

---
+++ xolotl/tests/solver/PetscSolverTester.cpp
@@ -0,0 +1,48 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Regression
+
+#include <boost/test/included/unit_test.hpp>
+#include <PSIClusterNetworkLoader.h>
+#include <memory>
+#include <typeinfo>
+#include <limits>
+#include <PSIClusterNetworkLoader.h>
+#include <PSIClusterReactionNetwork.h>
+#include <PetscSolver.h>
+#include "../../XolotlConfig.h"
+
+using namespace std;
+using namespace xolotlCore;
+
+/**
+ * The test suite configuration
+ */
+BOOST_AUTO_TEST_SUITE (PetscSolverTester_testSuite)
+
+/**
+ * This operation checks the fluxs from the reactant as best as is possible
+ * given that it requires external data.
+ */
+BOOST_AUTO_TEST_CASE(checkDOFConversion) {
+
+	// Local Declarations
+	string sourceDir(XolotlSourceDirectory);
+	string pathToFile("/tests/reactants/testfiles/tungsten.txt");
+	string networkFilename = sourceDir + pathToFile;
+
+	std::cout << networkFilename << std::endl;
+	// Load the input file from the master task
+	shared_ptr<std::istream> networkStream = make_shared<std::ifstream>(networkFilename);
+
+	// Create a network loader and set the istream on every MPI task
+	std::shared_ptr<PSIClusterNetworkLoader> networkLoader = make_shared<PSIClusterNetworkLoader>();
+	networkLoader->setInputstream(networkStream);
+	// Load the network
+	shared_ptr<ReactionNetwork> network = networkLoader->load();
+
+	cout << "Network loaded." << endl;
+
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
