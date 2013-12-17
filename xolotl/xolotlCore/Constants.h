b4f0b4f60651541fae8c8316b4b8dbce310cc9fb
jayjaybillings <jayjaybillings@mycompany.com>
2013-12-17 16:27:29 +0000

    Fixed erroneously lattice constant value. ~JJB 20131217 11:27
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/trunk@216 72105f29-0fe1-43bd-9363-b1e281e962a2

--- xolotl/xolotlCore/Constants.h
+++ xolotl/xolotlCore/Constants.h
@@ -18,8 +18,9 @@ static const double kBoltzmann = 8.6173324E-5;
 //! Pi
 static const double pi = 3.14159265359;
 
-//! Lattice Parameter
-static const double latticeConstant = 1.0;
+//! Lattice Parameter. Equal to 3.17 Angstroms, taken from Becquart et. al.
+// Journal of Nuclear Materials 403 (2010) 75–88. Given in units here of nm.
+static const double latticeConstant = 0.317;
 
 } /* end namespace xolotlCore */
 #endif /* CONSTANTS_H_ */
