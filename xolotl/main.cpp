0a5bb649c82562f3ae45972028ae41117016d628
jayjaybillings <jayjaybillings@mycompany.com>
2014-01-15 16:34:28 +0000

    Minor changes to the MPI stuff. ~JJB 20140115 11:36
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/trunk@235 72105f29-0fe1-43bd-9363-b1e281e962a2

--- xolotl/main.cpp
+++ xolotl/main.cpp
@@ -13,7 +13,6 @@
 using namespace std;
 using std::shared_ptr;
 
-
 //! This operation prints the start message
 void printStartMessage() {
 	cout << "Starting Xolotl Plasma-Surface Interactions Simulator" << endl;
@@ -32,54 +31,54 @@ int main(int argc, char **argv) {
 
 	// Local Declarations
 	shared_ptr<std::istream> networkStream;
+	std::shared_ptr<PSIClusterNetworkLoader> networkLoader;
 	int rank;
 
-	// Say hello
-	printStartMessage();
-	
 	// Check the arguments
 	if (argc < 2) {
 		cout << "Insufficient input provided! Aborting!" << std::endl;
 		printUsage();
 		return EXIT_FAILURE;
 	}
-	
+
 	// Extract the argument for the file name
 	const char *networkFilename = argv[1];
-	
+
 	try {
 		// Setup the solver
 		xolotlSolver::PetscSolver solver;
 		solver.setCommandLineOptions(argc, argv);
 		solver.initialize();
-		
+
 		// Get the MPI rank
 		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-		// Set the input stream on the master
+		// Setup the master
 		if (rank == 0) {
+			// Say hello
+			printStartMessage();
+			// Set the input stream on the master
 			networkStream = make_shared<std::ifstream>(networkFilename);
 		}
-		
+
 		// Broadcast the stream to all worker tasks
-		networkStream = xolotlCore::MPIUtils::broadcastStream(
-			networkStream, 0, MPI_COMM_WORLD);
-		
+		networkLoader = std::make_shared<
+					PSIClusterNetworkLoader>();
+		networkStream = xolotlCore::MPIUtils::broadcastStream(networkStream, 0,
+				MPI_COMM_WORLD );
+
 		// Create a network loader and set the stream on every MPI task
-		std::shared_ptr<PSIClusterNetworkLoader> networkLoader(
-			new PSIClusterNetworkLoader());
 		networkLoader->setInputstream(networkStream);
 		// Give the network loader to PETSc as input
 		solver.setNetworkLoader(networkLoader);
-		
+
 		// Launch the PetscSolver
 		solver.solve();
 		solver.finalize();
-	}
-	catch (std::string error) {
+	} catch (std::string & error) {
 		std::cout << error << std::endl;
 		std::cout << "Aborting." << std::endl;
 		return EXIT_FAILURE;
 	}
-	
+
 	return EXIT_SUCCESS;
 }
--- xolotl/xolotlCore/io/MPIUtils.cpp
+++ xolotl/xolotlCore/io/MPIUtils.cpp
@@ -8,51 +8,54 @@ using std::shared_ptr;
 
 
 shared_ptr<std::istream> MPIUtils::broadcastStream(
-	shared_ptr<std::istream> stream, int root, MPI_Comm comm) {
+	shared_ptr<std::istream> stream, int master, MPI_Comm comm) {
 	
 	int rank;
 	int tasks;
+	auto bufferSS = std::make_shared<std::stringstream>();
+	int bufferSize;
+	char *buffer;
+
+	// Get the rank and number of tasks
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &tasks);
 	
-	shared_ptr<std::stringstream> bufferSS(new std::stringstream);
-	int bufferSize;
-	char *buffer;
-	
 	// Master task
-	if (rank == root) {
+	if (rank == master) {
 		// Load the data from the input stream into memory
-		
 		// This method is clean and potentially bug-free, but it copies memory
 		// three times. However, this is not a problem for files under
 		// a few MiB, and reading from disk takes longer anyway.
-		
 		(*bufferSS) << stream->rdbuf();
 		std::string bufferString = bufferSS->str();
-		
+		// Create the new buffer and copy its contents into the string
 		bufferSize = bufferString.size();
 		buffer = new char[bufferSize];
 		bufferString.copy(buffer, bufferSize);
 	}
 	
-	MPI_Bcast(&bufferSize, 1, MPI_INT, root, MPI_COMM_WORLD);
+	// Broadcast the size
+	MPI_Bcast(&bufferSize, 1, MPI_INT, master, MPI_COMM_WORLD);
 	
-	if (rank != root) {
+	// Create a new buffer on the nodes
+	if (rank != master) {
 		buffer = new char[bufferSize];
 	}
 	
-	MPI_Bcast(buffer, bufferSize, MPI_CHAR, root, MPI_COMM_WORLD);
+	// Broadcast the buffer
+	MPI_Bcast(buffer, bufferSize, MPI_CHAR, master, MPI_COMM_WORLD);
 	
-	// Slave tasks
-	if (rank != root) {
+	// Creat the string from the buffer on the nodes
+	if (rank != master) {
 		std::string bufferString(buffer, bufferSize);
 		bufferSS->str(bufferString);
 	}
 	
+	// Rewind the stringstream before returning it
+	bufferSS->seekg(0);
+
+	// Clean up the buffer memory and return it
 	delete buffer;
 	
-	// Rewind the stringstream before returning it
-	
-	bufferSS->seekg(0);
 	return bufferSS;
 }
