466b337403af63680bd5f83a22af3f9085d34370
jayjaybillings <jayjaybillings@mycompany.com>
2014-06-13 19:20:53 +0000

    Added all of the HDF5 jar files and 64bit Linux libraries to the preproccessor project. ~JJB 20140613 15:22
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/trunk@494 72105f29-0fe1-43bd-9363-b1e281e962a2

--- xolotl/gov.ornl.xolotl.preprocessor/src/gov/ornl/xolotl/preprocessor/Main.java
+++ xolotl/gov.ornl.xolotl.preprocessor/src/gov/ornl/xolotl/preprocessor/Main.java
@@ -3,7 +3,11 @@
  */
 package gov.ornl.xolotl.preprocessor;
 
+import java.io.File;
 import java.util.ArrayList;
+
+import ncsa.hdf.object.FileFormat;
+import ncsa.hdf.object.h5.H5File;
 
 /**
  * This class launches the Xolotl preprocessor.
@@ -24,15 +28,31 @@ public class Main {
 
 		// Create the Preprocessor
 		Preprocessor preprocessor = new Preprocessor();
-		
+
 		// Generate the clusters
 		ArrayList<Cluster> clusters = preprocessor.generate(args);
-		
+
 		// Dump the clusters to stdout
 		for (Cluster cluster : clusters) {
 			System.out.println(cluster.toString());
 		}
-		
+
+		// Retrieve an instance of the HDF5 format
+		FileFormat fileFormat = FileFormat
+				.getFileFormat(FileFormat.FILE_TYPE_HDF5);
+
+		// Create a file from the uri
+		File file = new File("test.hdf5");
+
+		// Create an H5 file. If it exists already, then delete it.
+		try {
+			H5File h5File = (H5File) fileFormat.createFile(file.getPath(),
+					FileFormat.FILE_CREATE_DELETE);
+		} catch (Exception e) {
+			// Complain
+			e.printStackTrace();
+		}
+
 		return;
 	}
 
