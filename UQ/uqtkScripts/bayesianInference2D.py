68eece1d1a349e20b6b76a0dfa618f8905d69029
Sophie Blondel <sophie.blondel1@gmail.com>
2014-06-23 22:22:33 +0000

    Adding all the scripts needed to do bayesian inference in 2D. SB 20140623
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/trunk@527 72105f29-0fe1-43bd-9363-b1e281e962a2

---
+++ UQ/uqtkScripts/bayesianInference2D.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python
+#=====================================================================================
+#                     The UQ Toolkit (UQTk) version 2.0
+#                    Copyright (2013) Sandia Corporation
+#                     http://www.sandia.gov/UQToolkit/
+#
+#    Copyright (2013) Sandia Corporation. Under the terms of Contract DE-AC04-94AL85000
+#    with Sandia Corporation, the U.S. Government retains certain rights in this software.
+#
+#    This file is part of The UQ Toolkit (UQTk)
+#
+#    UQTk is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    UQTk is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with UQTk.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Questions? Contact Bert Debusschere <bjdebus@sandia.gov>
+#    Sandia National Laboratories, Livermore, CA, USA
+#====================================================================================
+
+
+import os
+import shutil
+import sys
+import numpy as np
+import math
+import random as rnd
+from scipy.stats.mstats import mquantiles
+import scipy.optimize
+import matplotlib.pyplot as plt
+
+import fileinput
+import file_utils
+
+from pylab import *
+
+rc('legend',loc='upper right', fontsize=20)
+rc('lines', linewidth=4, color='r')
+rc('axes',linewidth=3,grid=True,labelsize=22)
+rc('xtick',labelsize=16)
+rc('ytick',labelsize=16)
+
+###########################################################################
+# define uqtkbin
+if os.environ.get("UQTK_SRC") is None:
+    print "Error: Need to set path to uqtk src as environment variable UQTK_SRC -> Abort"
+    quit()
+else:
+    if ( not os.path.isdir(os.environ["UQTK_SRC"]) ):
+        print "\"",os.environ["UQTK_SRC"],"\" is not a valid path -> Abort"
+        quit()
+
+uqtkbin=os.environ["UQTK_SRC"]+"/src_cpp/bin"
+pcequad=uqtkbin+"/pce_quad"
+###########################################################################
+
+# Input settings 
+# They have to be changed to the value they have in the XML file
+xorder=5
+yorder=3
+
+chdimlist=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]      # Indices of chain parameters
+chstart=[69.734004, 50.788312, -6.280163, 0.674329, 49.214234, 37.250268, -3.675781, 0.671578, 20.428427, 18.915853, -0.516389, 0.329204, -1.647776, -0.460337, 0.766950, -0.208465, -1.370898, -1.764382, -0.413855, 0.006777, 0.507299, 0.650158, 0.241819, 0.108867, -0.737367]  # Starting value of the chain
+chainfile='chain.dat'      # Name of the chain file
+n_burnin=0              # Burnin samples
+
+## Prepare the xml file ##################################################
+shutil.copyfile('infer2D.xml.templ','infer2D.xml')
+for line in fileinput.input('infer2D.xml', inplace = 1):
+    print line.replace('CHAINFILE', chainfile),
+
+
+
+xin, yin, zin = loadtxt('inputdata2d.dat', usecols = (0,1,2), unpack=True)
+
+xin=2.*(xin/3.8125)-1.
+yin=2.*((yin-1)/43.)-1.
+np.savetxt("inputdata.dat", np.transpose([xin,yin,zin]))
+
+## Run the inference code #####################################################
+ii=len(chdimlist)
+for idim in chdimlist:
+    ii=ii-1
+    for line in fileinput.input("infer2D.xml", inplace = 1):
+        print line.replace('PAR_'+str(ii+1), str(chstart[ii])),
+ 
+print "Running the parameter inference"
+os.system('./infer2D.x')
+
+
+## Import data from MCMC file ###################################################
+print "Loading in chain file",chainfile
+all_samples, vnames = file_utils.extract_all_vars(chainfile,n_burnin,0,1)
+n_all_vars = len(vnames)
+n_cols = len(all_samples[0,:])
+# Extract all MCMC chain variables in separate array
+chn = all_samples[:,0:1+n_all_vars]
+nchn=chn.shape[0]
+
+
+## Plot chains ##################################################################
+for i in range(n_all_vars):
+    fig = plt.figure(figsize=(10,7))
+    ax=fig.add_axes([0.10,0.15,0.85,0.75])
+
+    plt.plot(chn[:,0],chn[:,i+1],color='black',linewidth=2)
+    ax.set_xlabel("MCMC step",fontsize=22)
+    ax.set_ylabel(vnames[i],fontsize=22)
+
+    plt.savefig('chn_'+vnames[i]+'.pdf')
+    plt.clf()
+
+for i in range(n_all_vars):
+    for j in range(i):
+        fig = plt.figure(figsize=(10,7))
+        ax=fig.add_axes([0.10,0.15,0.85,0.75])
+    
+        plt.plot(chn[:,j+1],chn[:,i+1],'ko',markeredgecolor='black',markersize=5)
+        ax.set_xlabel(vnames[j],fontsize=22)
+        ax.set_ylabel(vnames[i],fontsize=22)
+
+        plt.savefig('chn_'+vnames[j]+'_'+vnames[i]+'.pdf')
+        plt.clf()
+
+
+## Find posterior predictive #######################################################
+# ideally we should have a deterministic forward model code,
+# but for a line it is simple enough to compute here
+print "Sampling chain to compute posterior predictive distribution"
+param = np.zeros(len(chdimlist))
+
+ngr=len(yin)
+zpp=np.zeros((ngr))
+z2psh=np.zeros((ngr))
+zstd=np.zeros((ngr))
+for ip in range(nchn):
+    ii=0
+    for idim in chdimlist:
+        # grab variabl samples from posterior chain
+        param[idim-1]=chn[ip,ii+1]
+        ii=ii+1
+
+    # run forward model for these sampled parameter values
+    
+    for j in range(ngr):
+        leg = np.empty(len(chdimlist)-1)
+        i=0
+        for a in range(xorder+1):
+            for b in range(yorder+1):
+                leg[i]=scipy.special.eval_legendre(a,xin[j])*scipy.special.eval_legendre(b,yin[j])
+                i=i+1
+
+        newparam=param[:-1]
+        zval=np.dot(newparam,leg)
+        # running sum for y
+        zpp[j]=zpp[j]+zval
+        # running sum for y^2
+        z2psh[j]=z2psh[j]+zval**2
+        # running sigma
+        zstd[j]=zstd[j]+exp(param[len(chdimlist)-1])
+
+# pushed forward mean
+zpp=zpp/nchn
+# pushed forward ave y^2
+z2psh=z2psh/nchn
+
+zstd=zstd/nchn
+# pushed forward std dev
+std_push=(z2psh-zpp**2)**0.5
+# Posterior predictive std dev
+std_pp=(std_push**2 + zstd**2)**0.5
+
+# Loop on the list of V numbers
+Vlist = {1, 2, 6, 14, 18, 19, 27, 32, 44}
+
+# Plot pushed forward posterior and posterior predictive 
+fig = plt.figure(figsize=(10,7))
+ax=fig.add_axes([0.10,0.15,0.85,0.75])
+for v in Vlist:
+  # Filter everything
+  v=2.*((v-1)/43.)-1.
+  xinF = xin[yin == v]
+  zinF = zin[yin == v]
+  zppF = zpp[yin == v]
+  std_ppF = std_pp[yin == v]
+  std_pushF = std_push[yin == v]
+  yinF = yin[yin == v]
+  
+  plt.fill_between(xinF,zppF-std_ppF,zppF+std_ppF,color='lightgrey',label='Post predictive stdev')
+  plt.fill_between(xinF,zppF-std_pushF,zppF+std_pushF,color='grey',label='Pushed forward stdev')
+  plt.scatter(xinF, zppF, s=2, color='red', label='Mean prediction')
+  plt.plot(xinF, zinF,'o', markersize=4, color='black', label='Data')
+
+ax.set_xlabel("x",fontsize=22)
+ax.set_ylabel("y",fontsize=22)
+
+plt.savefig('postpred.pdf')
+plt.clf()
+
+# Plot pushed forward posterior and posterior predictive difference
+fig = plt.figure(figsize=(10,7))
+ax=fig.add_axes([0.10,0.15,0.85,0.75])
+for v in Vlist:
+  # Filter everything
+  v=2.*((v-1)/43.)-1.
+  xinF = xin[yin == v]
+  zinF = zin[yin == v]
+  zppF = zpp[yin == v]
+  std_ppF = std_pp[yin == v]
+  std_pushF = std_push[yin == v]
+  yinF = yin[yin == v]
+  
+  plt.fill_between(xinF,-std_ppF,std_ppF,color='lightgrey',label='Post predictive stdev')
+  plt.fill_between(xinF,-std_pushF,std_pushF,color='grey',label='Pushed forward stdev')
+  plt.plot(xinF, zppF-zppF, linewidth=1, color='red', label='Mean prediction')
+  plt.plot(xinF, zinF-zppF,'o', markersize=4, color='black', label='Data')
+
+ax.set_xlabel("x",fontsize=22)
+ax.set_ylabel("y",fontsize=22)
+
+plt.savefig('postpreddiff.pdf')
+plt.clf()
+
+
+## Plot posterior 'triangle' ####################################################
+# calling it as a script for now
+cmd='./plTriang2D.py -i ' + chainfile + ' --nb ' + str(n_burnin) + ' > plTriang.log'
+print "Running",cmd
+os.system(cmd)
+
+print "END: bayesianInference2D done: check out *.pdf files for results."
---
+++ UQ/uqtkScripts/plTriang2D.py
@@ -0,0 +1,279 @@
+#!/usr/bin/env python
+#=====================================================================================
+#                     The UQ Toolkit (UQTk) version 2.0
+#                    Copyright (2013) Sandia Corporation
+#                     http://www.sandia.gov/UQToolkit/
+#
+#    Copyright (2013) Sandia Corporation. Under the terms of Contract DE-AC04-94AL85000
+#    with Sandia Corporation, the U.S. Government retains certain rights in this software.
+#
+#    This file is part of The UQ Toolkit (UQTk)
+#
+#    UQTk is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    UQTk is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with UQTk.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Questions? Contact Bert Debusschere <bjdebus@sandia.gov>
+#    Sandia National Laboratories, Livermore, CA, USA
+#====================================================================================
+#
+# Given a set of samples of random variables, this script plots a lower triangular
+# matrix of marginalized densities. The diagonal contains the density of individual
+# random variables, marginalized over all other variables. Plots below the diagonal
+# contain the 2D density of the associated pair of random variables, marginalized over
+# all other variables.
+#
+# The script expects an input file containing samples of random variable samples. Each
+# column contains the samples of a different random variable. Each line is a sample
+# of the full random variable set.
+#
+# The first line of the file needs to contain labels for the respective columns
+#
+# The script is written for handling Markov Chain Monte Carlo (MCMC) posterior sample files,
+# so it assumes that the first column is an integer MCMC step number, and that the last two columns
+# contain the acceptance probability (labeled "acceptance_prob") and posterior probability. The script
+# can easily be modified to process files without those columns.
+#
+# Note 1) If the samples come from a full MCMC file, then the column labeled
+#   acceptance_prob will have a value of -1 on the last line. This line
+#   contains the MAP values of the chain, and must be removed from
+#   the sample list as it is not a regular sample. If the samples
+#   come from a partial MCMC run (e.g. a run in progress), then
+#   all lines need to be kept.
+# Note 2) All columns should be of equal length
+#
+# Written spring 2013 by Cosmin Safta and Bert Debusschere, Sandia National Labs, Livermore, CA
+# <bjdebus@sandia.gov>
+
+import os
+import sys
+import getopt
+import math
+
+import numpy as npy
+import matplotlib.pyplot as plt
+from scipy import stats, mgrid, c_, reshape, random, rot90
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+from scipy.stats import norm
+
+#sys.path.append('../../math_libs/pytools')
+import file_utils
+
+help_string = """
+Usage:
+  plot_triang.py [-h] -i <mcmc filename> [--nb <burn-in samples>]  [-s <stride>] [-d] [--nkde <n_pts_kde>]
+what
+  Plot MCMC variable marginal and covariance PDF graphs
+where
+  -h = print help info
+  -i = name of file containing MCMC data
+  -s = stride with which to read the file, default 1
+  --nb = number of burn-in samples to be removed from the chain, default 0
+  --nkde = number of points per dimension to evaluate densities at with KDE, default = 100
+           (more points gives a smoother PDF but increases the computational cost)
+""" 
+# Set to 1 to get more output to screen
+# Set to > 1 to get a lot of output to screen
+debug = 1
+
+# Set to 1 for showing plots interactively
+interact = 0
+
+
+#
+# Process inputs 
+#
+try:
+  opts, extra  = getopt.getopt(sys.argv[1:],"hi:s:d",["nb=","nkde="])
+except getopt.GetoptError, err:
+    print str(err)
+    print help_string
+    sys.exit(1)
+
+if (debug > 0):
+    print "Extra command line arguments following recognized arguments: ",extra
+
+# Default values
+samples_file_name=""
+n_burnin = 0
+stride = 1
+np_kde=100
+
+for o,a in opts:
+  if o == "-h":
+    print help_string
+    sys.exit(0)
+  elif o == "-i":
+    samples_file_name = a
+  elif o == "-s":
+    stride = int(a)
+  elif o == "--nb":
+    n_burnin = int(a)
+  elif o == "--nkde":
+    np_kde = int(a)
+  else:
+    assert False, "unhandled command line parsing option"
+
+# error checking
+if(samples_file_name==""):
+  print "Sample file name must be specified"
+  print help_string
+  sys.exit(1)
+
+if (n_burnin < 0):
+  print "The number of burn-in samples needs to be >= 0"
+  print help_string
+  sys.exit(1)
+
+if (stride < 0): 
+  print "The file read stride needs to be >= 0"
+  print help_string
+  sys.exit(1)
+
+if (np_kde < 1): 
+  print "The number of KDE points per dimension needs to be >= 1"
+  print help_string
+  sys.exit(1)
+
+# Base name of file for outputting results
+out_file_base = samples_file_name + ".nb" + str(n_burnin) + ".s" + str(stride)
+
+#
+# Import data from MCMC file
+#
+# Section 1
+# load chain file
+# it expects first column is line id, then the last
+# two columns are alpha and current log posterior 
+#d0 = npy.genfromtxt(chainfile)
+print "Loading in data file",samples_file_name
+all_samples, vnames = file_utils.extract_all_vars(samples_file_name,n_burnin,debug,stride)
+n_all_vars = len(vnames)
+n_cols = len(all_samples[0,:])
+
+# Extract all MCMC chain variables in separate array
+d0 = all_samples[:,1:1+n_all_vars]
+if (debug > 0):
+    print d0.shape
+
+# Some settings to connect with code Cosmin gave me
+nthin = 1                # take only every nthin state (for faster kde)
+nskip = 0                # entries to skip
+#npdf = 100               # no of grid points for 1D pdf's
+istart = 0               # number of column with first MCMC variable
+#cend   = 0               # number of extra columns at the end.
+cend   = 0               # remove columns at end so code runs faster for debugging
+
+nvars=d0.shape[1]-istart-cend  # number of variables we will actually process
+print 'Number of sample lines in file',d0.shape[0]
+print 'Number of vars  we will process in file',nvars
+
+# Section 2
+# set up 2D kde objects
+print "Setting up 2D KDE objects"
+kern_i_j=[]
+for j in range(istart+1,istart+nvars):
+    for i in range(istart,j):
+        if (debug > 2):
+            print i,j
+        kern_i_j.append(stats.kde.gaussian_kde(c_[d0[nskip::nthin,i],d0[nskip::nthin,j]].T))
+
+# Section 3
+# set up 2D meshes and evaluate kde objects on those meshes
+# no. of grid points is controlled with kde_idx, defaults to 100
+print "Evaluating 2D KDE objects on meshes. This may take a while ..."
+kde_idx = np_kde*1j # complex number to include end points
+xmesh=[]; ymesh=[]; zmesh=[];
+icount=0
+cov_idx = npy.zeros((nvars,nvars),dtype=npy.int) # 2D array to keep track of which index in xmesh etc. the 
+                                                   # the plots corresponding to vars i,j belong to
+for j in range(istart+1,istart+nvars):
+    for i in range(istart,j):
+        if (debug > 0):
+            print "Computing 2D marginal distribution between variables:",i,",",j,":",vnames[i]," & ",vnames[j]
+        x,y = mgrid[d0[nskip:,i].min():d0[nskip:,i].max():kde_idx, d0[nskip:,j].min():d0[nskip:,j].max():kde_idx]
+        z   = reshape(kern_i_j[icount](c_[x.ravel(), y.ravel()].T).T, x.T.shape)
+        xmesh.append(x);
+        ymesh.append(y);
+        zmesh.append(z);
+        cov_idx[i,j] = icount
+        icount = icount+1
+
+# Section 4
+# evaluate 1D pdfs
+print "Evaluating 1D marginal pdfs with KDE"
+xlin=[]; pdflin=[]; pdfgauss=[];
+for i in range(istart,istart+nvars):
+    xlin.append(npy.linspace(d0[nskip:,i].min(),d0[nskip:,i].max(),np_kde)) ;
+    kernlin=stats.kde.gaussian_kde(d0[nskip::nthin,i]);
+    mean, deviation = norm.fit(d0[nskip::nthin,i]);
+    pdflin.append(kernlin(xlin[i-istart]));
+    pdfgauss.append(norm.pdf(xlin[i-istart],mean,deviation));
+    
+    print i, mean, deviation
+
+# Section 5
+# Dense plot format: print full tri-diagonal matrix but w/o any white space, tick marks or lables.
+print "Assembling tri-diagonal plots in dense format"
+
+# ds is the distance between subplots
+# xs,ys are the coordinates (normalized) of the subplot in the lower left corner
+# xe,ye are the distances left in the uppper right corner
+# fsizex, fsizey are figure sizes
+# ncont are no of contours for 2D pdfs
+xs=0.12; ys=0.1; ds=0.0
+xe=0.08; ye=0.05
+fsizex=12; fsizey=12;
+ncont=10; 
+sx=(1-(nvars-1)*ds-xs-xe)/nvars;
+sy=(1-(nvars-1)*ds-ys-ye)/nvars;
+fs1=20
+majorFormatter = FormatStrFormatter('%6.0e')
+
+fig_d = plt.figure(figsize=(fsizex,fsizey))
+
+# Section 5.1
+subs=[]
+# add diagonal plots
+for i in range(nvars):
+    subs.append(fig_d.add_axes([xs+i*(sx+ds),ys+(nvars-1-i)*(sy+ds),sx,sy]))
+
+# add lower triangular plots
+for i in range(nvars-1):
+    for j in range(i+1):
+        if (debug > 2):
+            print j,(nvars-2-i)
+        subs.append(fig_d.add_axes([xs+j*(sx+ds),ys+(nvars-2-i)*(sy+ds),sx,sy]))
+
+subsnp=npy.array(subs)
+
+# Plot 1D pdfs
+for i in range(nvars):
+    subsnp[i].plot(xlin[i],pdflin[i])
+    # Gaussians
+    subsnp[i].plot(xlin[i],pdfgauss[i],c=(1,0,0))  
+
+# Plot 2D pdfs
+for i in range(nvars*(nvars-1)/2):
+    subsnp[nvars+i].contour(xmesh[i],ymesh[i],zmesh[i],ncont)
+
+# Section 5.2
+# no ticks and ticklabels
+for subpl in subsnp:
+    subpl.set_xticks([]); 
+    subpl.set_yticks([]); 
+
+plt.savefig("posteriors.pdf")
+
+
+
+
