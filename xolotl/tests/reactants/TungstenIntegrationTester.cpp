b9807ef66a593892df9e7407bd28af48337bbb82
Sophie Blondel <sophie.blondel1@gmail.com>
2014-08-25 20:17:34 +0000

    Adding a more thorough test for the partial derivatives in TungstenIntegrationTester.  SB 20140825
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/trunk@649 72105f29-0fe1-43bd-9363-b1e281e962a2

--- xolotl/tests/reactants/TungstenIntegrationTester.cpp
+++ xolotl/tests/reactants/TungstenIntegrationTester.cpp
@@ -69,17 +69,22 @@ BOOST_AUTO_TEST_CASE(checkGetReactantFluxesAndParials) {
 	auto reactants = network->getAll();
 	network->setTemperature(temperature);
 
+	// Initialize all the concentrations to 0.001;
+	for (int i = 0; i < nReactants; ++i) {
+		auto reactant = (PSICluster *) reactants->at(i);
+		reactant->setConcentration(0.001);
+	}
+
 	BOOST_TEST_MESSAGE("TungstenIntegrationTester Message: "
 			<< "Size of the network is: "
 			<< nReactants);
 
 	// Create an array to test the second partial derivative routine.
-	auto secondPartials = std::vector<double>(nReactants,0.0);
+	auto secondPartials = std::vector<double>(nReactants, 0.0);
 
 	BOOST_TEST_MESSAGE("Check partial derivatives.");
 	for (int i = 0; i < nReactants; ++i) {
 		auto reactant = (PSICluster *) reactants->at(i);
-		double flux = reactant->getTotalFlux(temperature);
 		// Get the partials using method 1
 		auto partials = reactant->getPartialDerivatives(temperature);
 		// Get the partials using method 2
@@ -91,6 +96,67 @@ BOOST_AUTO_TEST_CASE(checkGetReactantFluxesAndParials) {
 		// Zero the partials array
 		std::fill(secondPartials.begin(),secondPartials.end(),0.0);
 	}
-
 }
+
+ /**
+  * This operation checks the partial derivatives for the production
+  * He_1 + He_1 --> He_2
+  * and the dissociation
+  * He_2 --> He_1 + He_1
+  */
+ BOOST_AUTO_TEST_CASE(checkSingleReaction) {
+ 	// Local Declarations
+ 	string sourceDir(XolotlSourceDirectory);
+ 	// This file contains specific values to obtain round numbers for the partial derivatives
+ 	string pathToFile("/tests/reactants/testfiles/single_reaction.txt");
+ 	string networkFilename = sourceDir + pathToFile;
+
+ 	BOOST_TEST_MESSAGE(
+ 			"TungstenIntegrationTester Message: Network filename is: " << networkFilename);
+
+	// Load the input file from the master task
+	shared_ptr<istream> networkStream = make_shared < ifstream
+			> (networkFilename);
+
+	// Create a network loader and set the istream on every MPI task
+	shared_ptr<PSIClusterNetworkLoader> networkLoader = make_shared
+			< PSIClusterNetworkLoader
+			> (std::make_shared<xolotlPerf::DummyHandlerRegistry>());
+	networkLoader->setInputstream(networkStream);
+	// Load the network
+	shared_ptr<ReactionNetwork> network = networkLoader->load();
+
+	BOOST_TEST_MESSAGE("TungstenIntegrationTester Message: Network loaded");
+
+	// Get everything from the network and set the temperature
+	double temperature = 1000.0;
+	int nReactants = network->size();
+	auto reactants = network->getAll();
+	network->setTemperature(temperature);
+
+	// Initialize all the concentrations to 0.001;
+	for (int i = 0; i < nReactants; ++i) {
+		auto reactant = (PSICluster *) reactants->at(i);
+		reactant->setConcentration(0.001);
+	}
+
+	// Get He_1
+	auto reactant = (PSICluster *) reactants->at(0);
+	// Its partial derivatives
+	auto partials = reactant->getPartialDerivatives(temperature);
+
+	// Check the values of the partial derivatives
+	BOOST_REQUIRE_CLOSE(partials[0], -2.0, 0.1);
+	BOOST_REQUIRE_CLOSE(partials[1], 1.0, 0.1);
+
+	// Get He_2
+	reactant = (PSICluster *) reactants->at(1);
+	// Its partial derivatives
+	partials = reactant->getPartialDerivatives(temperature);
+
+	// Check the values of the partial derivatives
+	BOOST_REQUIRE_CLOSE(partials[0], 1.0, 0.1);
+	BOOST_REQUIRE_CLOSE(partials[1], -0.5, 0.1);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
