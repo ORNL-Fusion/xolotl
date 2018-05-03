02fb8d3a2f20d25f738f9c11161974213d6ac86d
Sophie Blondel <sophie.blondel1@gmail.com>
2018-05-03 14:58:34 -0400

    Added a few tests for the PSISuperCluster. SB 20180503

--- xolotl/tests/reactants/PSISuperClusterTester.cpp
+++ xolotl/tests/reactants/PSISuperClusterTester.cpp
@@ -57,7 +57,7 @@ BOOST_AUTO_TEST_CASE(checkConnectivity) {
 	// Recompute Ids and network size and redefine the connectivities
 	network->reinitializeConnectivities();
 
-	// Check the reaction connectivity of the super cluster
+	// Get the super cluster
 	auto& reactant = network->getAll(ReactantType::PSISuper).begin()->second;
 
 	// Check the type name
@@ -67,7 +67,6 @@ BOOST_AUTO_TEST_CASE(checkConnectivity) {
 	// Check the connectivity for He, V, and I
 	int connectivityExpected[] = { 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
 			1, 1, 1, 1, 0 };
-
 	for (unsigned int i = 0; i < reactionConnectivity.size(); i++) {
 		BOOST_REQUIRE_EQUAL(reactionConnectivity[i], connectivityExpected[i]);
 	}
@@ -107,7 +106,7 @@ BOOST_AUTO_TEST_CASE(checkTotalFlux) {
 	// Recompute Ids and network size and redefine the connectivities
 	network->reinitializeConnectivities();
 
-	// Check the reaction connectivity of the super cluster
+	// Get the super cluster
 	auto& cluster = network->getAll(ReactantType::PSISuper).begin()->second;
 	// Get one that it combines with (He)
 	auto secondCluster = (PSICluster *) network->get(Species::He, 1);
@@ -154,10 +153,9 @@ BOOST_AUTO_TEST_CASE(checkPartialDerivatives) {
 	// Recompute Ids and network size and redefine the connectivities
 	network->reinitializeConnectivities();
 
-	// Check the reaction connectivity of the super cluster
+	// Get the super cluster
 	auto& cluster = network->getAll(ReactantType::PSISuper).begin()->second;
 
-	// Local Declarations
 	// The vector of partial derivatives to compare with
 	double knownPartials[] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
 			0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -2.24076e-19, 0.0, 0.0, 0.0 };
@@ -202,11 +200,102 @@ BOOST_AUTO_TEST_CASE(checkReactionRadius) {
 	// Load the network
 	auto network = loader.load(opts);
 
-	// Check the reaction connectivity of the super cluster
+	// Get the super cluster
 	auto& cluster = network->getAll(ReactantType::PSISuper).begin()->second;
 
 	// Check the radius
 	BOOST_REQUIRE_CLOSE(0.137265, cluster->getReactionRadius(), 0.001);
+
+	return;
+}
+
+/**
+ * This operation checks the get concentration methods for PSISuperCluster.
+ */
+BOOST_AUTO_TEST_CASE(checkGetConcentrations) {
+
+	// Create the network loader
+	HDF5NetworkLoader loader = HDF5NetworkLoader(
+			make_shared<xolotlPerf::DummyHandlerRegistry>());
+	// Define the filename to load the network from
+	string sourceDir(XolotlSourceDirectory);
+	string pathToFile("/tests/testfiles/tungsten_diminutive_2D.h5");
+	string filename = sourceDir + pathToFile;
+	// Give the filename to the network loader
+	loader.setFilename(filename);
+	// Set grouping parameters
+	loader.setVMin(1);
+	loader.setWidth(4, 0);
+	loader.setWidth(1, 3);
+
+	// Create the options needed to load the network
+	Options opts;
+	// Load the network
+	auto network = loader.load(opts);
+
+	// Get a super cluster
+	auto& cluster = network->getAll(ReactantType::PSISuper).begin()->second;
+	auto& superCluster =
+			static_cast<PSISuperCluster&>(*cluster);
+
+	// Set the concentration in the cluster
+	superCluster.setZerothMoment(0.5);
+	superCluster.setMoment(1.0, 0); // He
+	superCluster.setMoment(1.0, 3); // V
+
+	// Check the different concentrations
+	BOOST_REQUIRE_CLOSE(0.5, superCluster.getTotalConcentration(), 0.001);
+	BOOST_REQUIRE_CLOSE(4.5, superCluster.getTotalAtomConcentration(0), 0.001); // He
+	BOOST_REQUIRE_CLOSE(0.5, superCluster.getTotalVacancyConcentration(), 0.001);
+	BOOST_REQUIRE_CLOSE(0.5, superCluster.getIntegratedVConcentration(1), 0.001);
+
+	return;
+}
+
+/**
+ * This operation checks the boundary methods for PSISuperCluster.
+ */
+BOOST_AUTO_TEST_CASE(checkBoundaries) {
+
+	// Create the network loader
+	HDF5NetworkLoader loader = HDF5NetworkLoader(
+			make_shared<xolotlPerf::DummyHandlerRegistry>());
+	// Define the filename to load the network from
+	string sourceDir(XolotlSourceDirectory);
+	string pathToFile("/tests/testfiles/tungsten_diminutive_2D.h5");
+	string filename = sourceDir + pathToFile;
+	// Give the filename to the network loader
+	loader.setFilename(filename);
+	// Set grouping parameters
+	loader.setVMin(1);
+	loader.setWidth(4, 0);
+	loader.setWidth(1, 3);
+
+	// Create the options needed to load the network
+	Options opts;
+	// Load the network
+	auto network = loader.load(opts);
+
+	// Get a super cluster
+	auto& cluster = network->getAll(ReactantType::PSISuper).begin()->second;
+	auto& superCluster =
+			static_cast<PSISuperCluster&>(*cluster);
+
+	// Check the different numbers
+	BOOST_REQUIRE_CLOSE(1.0, superCluster.getNumV(), 0.001);
+	BOOST_REQUIRE_CLOSE(1.0, superCluster.getNTot(), 0.001);
+	auto bounds = superCluster.getBounds(0); // He
+	BOOST_REQUIRE_EQUAL(9, *(bounds.begin()));
+	BOOST_REQUIRE_EQUAL(10, *(bounds.end()));
+	bounds = superCluster.getBounds(1); // D
+	BOOST_REQUIRE_EQUAL(0, *(bounds.begin()));
+	BOOST_REQUIRE_EQUAL(1, *(bounds.end()));
+	bounds = superCluster.getBounds(2); // T
+	BOOST_REQUIRE_EQUAL(0, *(bounds.begin()));
+	BOOST_REQUIRE_EQUAL(1, *(bounds.end()));
+	bounds = superCluster.getBounds(3); // V
+	BOOST_REQUIRE_EQUAL(1, *(bounds.begin()));
+	BOOST_REQUIRE_EQUAL(2, *(bounds.end()));
 
 	// Finalize MPI
 	MPI_Finalize();
