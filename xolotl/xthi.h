15580807e867fd7d2aef8f8e1b721323ad82fec4
Philip C. Roth <rothpc@ornl.gov>
2018-07-24 11:11:57 -0400

    Added ability to print out process affinity settings.

---
+++ xolotl/xthi.h
@@ -0,0 +1,15 @@
+#ifndef XTHI_H
+#define XTHI_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif // defined(__cplusplus)
+
+void xthi(void);
+
+#if defined(__cplusplus)
+}
+#endif // defined(__cplusplus)
+
+#endif // XTHI_H
+
