4a1b0052c373674ac5aafbd18fff9641158e2f1d
Sophie Blondel <sophie.blondel1@gmail.com>
2018-08-24 08:50:00 -0400

    Adding a factor 4.0 in the re-solution rate to convert from Xe rate to fission rate. SB 20180824

--- xolotl/xolotlCore/modifiedreaction/resolution/ReSolutionHandler.cpp
+++ xolotl/xolotlCore/modifiedreaction/resolution/ReSolutionHandler.cpp
@@ -134,7 +134,7 @@ void ReSolutionHandler::initialize(const IReactionNetwork& network) {
 
 void ReSolutionHandler::updateReSolutionRate(double rate) {
 	// Scale the fission rate with the standard one
-	resolutionRate = 1.0e8 * rate;
+	resolutionRate = 1.0e8 * 4.0 * rate; // The 4.0 factor is to get from the Xe rate to the fission rate
 
 	return;
 }
