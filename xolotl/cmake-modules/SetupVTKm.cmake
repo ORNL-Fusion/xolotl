453041e4fd3b65544587edfd8287897680a61fea
James <james@jameskress.com>
2019-03-15 11:05:23 -0400

    Moving VTKm to master branch to fix matrix inverse error.

--- xolotl/cmake-modules/SetupVTKm.cmake
+++ xolotl/cmake-modules/SetupVTKm.cmake
@@ -1,10 +1,18 @@
 # use VTKM_DIR to setup the options that cmake's find VTKm needs
-set(VTKm_DIR ${VTKM_DIR}/lib/cmake/vtkm-1.3)
-MESSAGE(STATUS "Looking for VTKm using VTKM_DIR = ${VTKm_DIR}")
+if(NOT VTKM_DIR)
+    MESSAGE(FATAL_ERROR "VTKm support needs explicit VTKM_DIR")
+endif()
 
-find_package(VTKm REQUIRED QUIET)
+MESSAGE(STATUS "Looking for VTKm using VTKM_DIR = ${VTKM_DIR}")
+
+# use VTKM_DIR to setup the options that cmake's find VTKm needs
+file(GLOB VTKm_DIR "${VTKM_DIR}/lib/cmake/vtkm-*")
+if(NOT VTKm_DIR)
+    MESSAGE(FATAL_ERROR "Failed to find VTKm at VTKM_DIR=${VTKM_DIR}/lib/cmake/vtk-*")
+endif()
+
+find_package(VTKm REQUIRED)
 set(VTKm_LIBRARIES vtkm_cont vtkm_rendering)
-set(VTKm_INCLUDE_DIRS "${VTKm_DIR}/include/vtkm-1.3 ${VTKm_DIR}/vtkm/thirdparty/taotuple")
 set(VTKM_FOUND TRUE)
 
 message(STATUS "VTKm includes = ${VTKm_INCLUDE_DIRS}")
