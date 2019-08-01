e9bff7940ccaf0d9169f5dd52487c1432dd0cd33
Samuel Morris <smorri20@vols.utk.edu>
2019-08-01 09:09:14 -0400

    Updated vacancy migration energy to match SPICES. SM 20190801

--- xolotl/xolotlCore/reactants/alloyclusters/AlloyFaultedCluster.h
+++ xolotl/xolotlCore/reactants/alloyclusters/AlloyFaultedCluster.h
@@ -55,7 +55,7 @@ public:
 		formationEnergy = _network.getFormationEnergy(type, size);
 
 		// Define the migration energy
-		migrationEnergy = 1.2;
+		migrationEnergy = 1.3;
 
 		// Define the reaction radius
 		reactionRadius = _network.getReactionRadius(type, size);
