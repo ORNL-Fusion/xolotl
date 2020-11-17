d4f86a8275492c8067b34ab7fb853904bfff110b
Philip Fackler <facklerpw@ornl.gov>
2020-11-17 12:19:40 -0500

    Updated exception handling in interface

--- xolotl/interface/src/Interface.cpp
+++ xolotl/interface/src/Interface.cpp
@@ -59,6 +59,12 @@ private:
 	Kokkos::ScopeGuard _kokkosContext;
 };
 
+void
+reportException(const std::exception& e)
+{
+	std::cerr << e.what() << "\nAborting." << std::endl;
+}
+
 XolotlInterface::XolotlInterface() = default;
 
 XolotlInterface::XolotlInterface(int argc, char* argv[], MPI_Comm mpiComm)
@@ -82,7 +88,7 @@ XolotlInterface::printSomething()
 
 void
 XolotlInterface::initializeXolotl(int argc, char* argv[], MPI_Comm comm)
-{
+try {
 	context = std::make_unique<Context>(argc, argv);
 
 	// Initialize the MPI communicator to use
@@ -103,388 +109,209 @@ XolotlInterface::initializeXolotl(int argc, char* argv[], MPI_Comm comm)
 		std::cout << std::asctime(std::localtime(&currentTime));
 	}
 
-	try {
-		options::Options opts;
-		opts.readParams(argc, argv);
-		if (!opts.shouldRun()) {
-			std::cerr << "Unable to read the options.  Aborting" << std::endl;
-		}
+	options::Options opts;
+	opts.readParams(argc, argv);
+	if (!opts.shouldRun()) {
+		throw std::runtime_error("Unable to read the options.");
+	}
 
-		// Set up our performance data infrastructure.
-		perf::PerfHandlerRegistry::set(
-			factory::perf::PerfHandlerFactory::get().generate(opts));
+	// Set up our performance data infrastructure.
+	perf::PerfHandlerRegistry::set(
+		factory::perf::PerfHandlerFactory::get().generate(opts));
 
-		// Initialize the visualization
-		viz::VizHandlerRegistry::set(
-			factory::viz::VizHandlerFactory::get().generate(opts));
+	// Initialize the visualization
+	viz::VizHandlerRegistry::set(
+		factory::viz::VizHandlerFactory::get().generate(opts));
 
-		// Setup the solver
-		solver = factory::solver::SolverFactory::get().generate(opts);
-		assert(solver);
-		// Initialize the solver
-		solver->initialize();
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
+	// Setup the solver
+	solver = factory::solver::SolverFactory::get().generate(opts);
+	assert(solver);
+	// Initialize the solver
+	solver->initialize();
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 void
 XolotlInterface::setTimes(double finalTime, double dt)
-{
-	try {
-		// Set the time in the solver
-		solver->setTimes(finalTime, dt);
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
+try {
+	// Set the time in the solver
+	solver->setTimes(finalTime, dt);
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 void
 XolotlInterface::solveXolotl()
-{
-	try {
-		// Launch the PetscSolver
-		solver->solve();
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
+try {
+	// Launch the PetscSolver
+	solver->solve();
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 std::vector<std::vector<std::vector<std::array<double, 4>>>>
 XolotlInterface::getLocalNE()
-{
-	std::vector<std::vector<std::vector<std::array<double, 4>>>> toReturn;
-	try {
-		// Get the solver handler
-		auto& solverHandler = solver::Solver::getSolverHandler();
-		// Get the rate vector
-		toReturn = solverHandler.getLocalNE();
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-
-	return toReturn;
+try {
+	// Get the solver handler and return the rate vector
+	return solver::Solver::getSolverHandler().getLocalNE();
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 void
 XolotlInterface::setLocalNE(
 	const std::vector<std::vector<std::vector<std::array<double, 4>>>>&
 		rateVector)
-{
-	try {
-		// Get the solver handler
-		auto& solverHandler = solver::Solver::getSolverHandler();
-		// Set the rate vector
-		solverHandler.setLocalNE(rateVector);
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
+try {
+	// Get the solver handler
+	auto& solverHandler = solver::Solver::getSolverHandler();
+	// Set the rate vector
+	solverHandler.setLocalNE(rateVector);
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 void
 XolotlInterface::getLocalCoordinates(int& xs, int& xm, int& Mx, int& ys,
 	int& ym, int& My, int& zs, int& zm, int& Mz)
-{
-	try {
-		// Get the solver handler
-		auto& solverHandler = solver::Solver::getSolverHandler();
-		// Get the local coordinates
-		solverHandler.getLocalCoordinates(xs, xm, Mx, ys, ym, My, zs, zm, Mz);
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
+try {
+	// Get the solver handler
+	auto& solverHandler = solver::Solver::getSolverHandler();
+	// Get the local coordinates
+	solverHandler.getLocalCoordinates(xs, xm, Mx, ys, ym, My, zs, zm, Mz);
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 void
 XolotlInterface::setGBLocation(int i, int j, int k)
-{
-	try {
-		// Get the solver handler
-		auto& solverHandler = solver::Solver::getSolverHandler();
-		// Set the coordinate of the GB
-		solverHandler.setGBLocation(i, j, k);
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
+try {
+	// Get the solver handler
+	auto& solverHandler = solver::Solver::getSolverHandler();
+	// Set the coordinate of the GB
+	solverHandler.setGBLocation(i, j, k);
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 void
 XolotlInterface::resetGBVector()
-{
-	try {
-		// Get the solver handler
-		auto& solverHandler = solver::Solver::getSolverHandler();
-		// Reset the location
-		solverHandler.resetGBVector();
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
+try {
+	// Get the solver handler
+	auto& solverHandler = solver::Solver::getSolverHandler();
+	// Reset the location
+	solverHandler.resetGBVector();
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 std::vector<std::vector<std::vector<std::vector<std::pair<int, double>>>>>
 XolotlInterface::getConcVector()
-{
-	std::vector<std::vector<std::vector<std::vector<std::pair<int, double>>>>>
-		toReturn;
-	try {
-		// Get the vector
-		toReturn = solver->getConcVector();
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-
-	return toReturn;
+try {
+	// Get the vector
+	return solver->getConcVector();
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 void
 XolotlInterface::setConcVector(
 	std::vector<std::vector<std::vector<std::vector<std::pair<int, double>>>>>
 		concVector)
-{
-	try {
-		// Set the vector
-		solver->setConcVector(concVector);
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
+try {
+	// Set the vector
+	solver->setConcVector(concVector);
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 double
 XolotlInterface::getPreviousTime()
-{
-	double toReturn = 0.0;
-	try {
-		// Get the solver handler
-		auto& solverHandler = solver::Solver::getSolverHandler();
-		toReturn = solverHandler.getPreviousTime();
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-
-	return toReturn;
+try {
+	// Get the solver handler
+	auto& solverHandler = solver::Solver::getSolverHandler();
+	return solverHandler.getPreviousTime();
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 void
 XolotlInterface::setPreviousTime(double time)
-{
-	try {
-		// Get the solver handler
-		auto& solverHandler = solver::Solver::getSolverHandler();
-		solverHandler.setPreviousTime(
-			time, true); // Update the fluence from here
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
+try {
+	// Get the solver handler
+	auto& solverHandler = solver::Solver::getSolverHandler();
+	solverHandler.setPreviousTime(time, true); // Update the fluence from here
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 double
 XolotlInterface::getCurrentDt()
-{
-	double toReturn = 0.0;
-	try {
-		toReturn = solver->getCurrentDt();
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-
-	return toReturn;
+try {
+	return solver->getCurrentDt();
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 void
 XolotlInterface::setCurrentTimes(double time, double dt)
-{
-	try {
-		solver->setCurrentTimes(time, dt);
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
+try {
+	solver->setCurrentTimes(time, dt);
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 double
 XolotlInterface::getNXeGB()
-{
-	double toReturn = 0.0;
-	try {
-		// Get the solver handler
-		auto& solverHandler = solver::Solver::getSolverHandler();
-		toReturn = solverHandler.getNXeGB();
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-
-	return toReturn;
+try {
+	// Get the solver handler
+	auto& solverHandler = solver::Solver::getSolverHandler();
+	return solverHandler.getNXeGB();
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 void
 XolotlInterface::setNXeGB(double nXe)
-{
-	try {
-		// Get the solver handler
-		auto& solverHandler = solver::Solver::getSolverHandler();
-		solverHandler.setNXeGB(nXe);
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
+try {
+	// Get the solver handler
+	auto& solverHandler = solver::Solver::getSolverHandler();
+	solverHandler.setNXeGB(nXe);
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 TS&
@@ -495,97 +322,60 @@ XolotlInterface::getTS()
 
 std::vector<double>
 XolotlInterface::getGridInfo(double& hy, double& hz)
-{
-	// The vector to return
-	std::vector<double> toReturn;
-	try {
-		// Get the solver handler
-		auto& solverHandler = solver::Solver::getSolverHandler();
-		// Get the grid
-		toReturn = solverHandler.getXGrid();
-		// Get the step size
-		hy = solverHandler.getStepSizeY();
-		hz = solverHandler.getStepSizeZ();
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
+try {
+	// Get the solver handler
+	auto& solverHandler = solver::Solver::getSolverHandler();
 
-	return toReturn;
+	// Get the step size
+	hy = solverHandler.getStepSizeY();
+	hz = solverHandler.getStepSizeZ();
+
+	return solverHandler.getXGrid();
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 bool
 XolotlInterface::getConvergenceStatus()
-{
-	bool toReturn = true;
-	try {
-		toReturn = solver->getConvergenceStatus();
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-
-	return toReturn;
+try {
+	return solver->getConvergenceStatus();
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 void
 XolotlInterface::finalizeXolotl()
-{
-	try {
-		// Call solver finalize
-		solver->finalize();
+try {
+	// Call solver finalize
+	solver->finalize();
 
-		// Report statistics about the performance data collected during
-		// the run we just completed.
-		auto handlerRegistry = perf::PerfHandlerRegistry::get();
-		perf::PerfObjStatsMap<perf::ITimer::ValType> timerStats;
-		perf::PerfObjStatsMap<perf::IEventCounter::ValType> counterStats;
-		perf::PerfObjStatsMap<perf::IHardwareCounter::CounterType> hwCtrStats;
-		handlerRegistry->collectStatistics(
-			timerStats, counterStats, hwCtrStats);
+	// Report statistics about the performance data collected during
+	// the run we just completed.
+	auto handlerRegistry = perf::PerfHandlerRegistry::get();
+	perf::PerfObjStatsMap<perf::ITimer::ValType> timerStats;
+	perf::PerfObjStatsMap<perf::IEventCounter::ValType> counterStats;
+	perf::PerfObjStatsMap<perf::IHardwareCounter::CounterType> hwCtrStats;
+	handlerRegistry->collectStatistics(timerStats, counterStats, hwCtrStats);
 
-		auto xolotlComm = util::getMPIComm();
+	auto xolotlComm = util::getMPIComm();
 
-		// Get the MPI rank
-		int rank;
-		MPI_Comm_rank(xolotlComm, &rank);
-		if (rank == 0) {
-			handlerRegistry->reportStatistics(
-				std::cout, timerStats, counterStats, hwCtrStats);
-		}
+	// Get the MPI rank
+	int rank;
+	MPI_Comm_rank(xolotlComm, &rank);
+	if (rank == 0) {
+		handlerRegistry->reportStatistics(
+			std::cout, timerStats, counterStats, hwCtrStats);
+	}
 
-		solver.reset();
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (const std::string& error) {
-		std::cerr << error << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
-	catch (...) {
-		std::cerr << "Unrecognized exception seen." << std::endl;
-		std::cerr << "Aborting." << std::endl;
-	}
+	solver.reset();
+}
+catch (const std::exception& e) {
+	reportException(e);
+	throw;
 }
 
 } /* namespace interface */
