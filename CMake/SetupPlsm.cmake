fbfe127ab8bdc35848f9e42fb15c6dd724166536
Philip Fackler <facklerpw@ornl.gov>
2021-05-26 12:58:07 -0400

    Use actual submodule commit reference to checkout instead of calling update (#44)

--- CMake/SetupPlsm.cmake
+++ CMake/SetupPlsm.cmake
@@ -35,7 +35,8 @@ set(__plsm_bin_dir ${__external_bin_dir}/plsm_build)
 execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${__plsm_bin_dir})
 execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${__plsm_bin_dir})
 
-function(init_plsm_submodule)
+if(NOT EXISTS ${__plsm_src_dir})
+    message(STATUS "    checkout")
     execute_process(
         COMMAND ${GIT_EXECUTABLE} submodule update --init
         WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
@@ -48,16 +49,12 @@ function(init_plsm_submodule)
         OUTPUT_FILE "${__external_bin_dir}/plsm_clone.out"
         ERROR_FILE "${__external_bin_dir}/plsm_clone.out"
     )
-endfunction()
-
-if(NOT EXISTS ${__plsm_src_dir})
-    message(STATUS "    checkout")
-    init_plsm_submodule()
 endif()
 
 # Should we update the source to a different version
 set(XOLOTL_USE_PLSM_DEVELOP ${Xolotl_USE_PLSM_DEVELOP} CACHE INTERNAL "")
 if(__do_src_update)
+    message(STATUS "    checkout")
     if(${Xolotl_USE_PLSM_DEVELOP})
         execute_process(
             COMMAND ${GIT_EXECUTABLE} checkout origin/develop
@@ -66,7 +63,23 @@ if(__do_src_update)
             ERROR_FILE "${__external_bin_dir}/plsm_clone.out"
         )
     else()
-        init_plsm_submodule()
+        # Get original submodule commit and checkout
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} ls-tree HEAD:external
+            WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+            OUTPUT_VARIABLE __external_ls_output
+        )
+        separate_arguments(__external_ls_output
+            NATIVE_COMMAND
+            ${__external_ls_output}
+        )
+        list(GET __external_ls_output 2 __ref_commit)
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} checkout ${__ref_commit}
+            WORKING_DIRECTORY "${__plsm_src_dir}"
+            OUTPUT_FILE "${__external_bin_dir}/plsm_clone.out"
+            ERROR_FILE "${__external_bin_dir}/plsm_clone.out"
+        )
     endif()
 endif()
 
