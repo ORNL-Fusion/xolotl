3c7bbbf35955a2cbdb0ebf3ff16d19347964d57b
Philip Fackler <facklerpw@ornl.gov>
2021-09-03 14:08:17 -0400

    Use custom log level and enable named scopes (#30)

--- xolotl/util/include/xolotl/util/Log.h
+++ xolotl/util/include/xolotl/util/Log.h
@@ -3,23 +3,29 @@
 #include <iomanip>
 #include <sstream>
 
+#include <boost/log/attributes/named_scope.hpp>
 #include <boost/log/sources/global_logger_storage.hpp>
 #include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/trivial.hpp>
+#include <boost/log/sources/record_ostream.hpp>
 
 namespace xolotl
 {
 namespace util
 {
-using LogLevel = boost::log::trivial::severity_level;
-// enum LogLevel
-// {
-// 	debug,
-// 	note,
-// 	info,
-// 	warning,
-// 	error
-// };
+enum LogLevel
+{
+	debug,
+	extra,
+	info,
+	warning,
+	error
+};
+
+const char*
+toString(LogLevel level);
+
+std::ostream&
+operator<<(std::ostream& os, LogLevel level);
 
 using LoggerType = boost::log::sources::severity_logger<LogLevel>;
 
@@ -54,5 +60,6 @@ public:
 #define XOLOTL_LOG XOLOTL_LOG_SEV(::xolotl::util::LogLevel::info)
 
 #define XOLOTL_LOG_DBG XOLOTL_LOG_SEV(::xolotl::util::LogLevel::debug)
+#define XOLOTL_LOG_XTRA XOLOTL_LOG_SEV(::xolotl::util::LogLevel::extra)
 #define XOLOTL_LOG_WARN XOLOTL_LOG_SEV(::xolotl::util::LogLevel::warning)
 #define XOLOTL_LOG_ERR XOLOTL_LOG_SEV(::xolotl::util::LogLevel::error)
--- xolotl/util/src/Log.cpp
+++ xolotl/util/src/Log.cpp
@@ -1,6 +1,10 @@
+#include <boost/log/attributes.hpp>
 #include <boost/log/core.hpp>
 #include <boost/log/expressions.hpp>
+#include <boost/log/expressions/formatters/named_scope.hpp>
 #include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/log/support/exception.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/console.hpp>
 #include <boost/log/utility/setup/file.hpp>
@@ -11,6 +15,37 @@ namespace xolotl
 {
 namespace util
 {
+BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", LogLevel)
+
+const char*
+toString(LogLevel level)
+{
+	static const char* strings[] = {
+		"DEBUG", "EXTRA", "INFO", "WARNING", "ERROR"};
+
+	auto l = static_cast<std::size_t>(level);
+	if (l < sizeof(strings) / sizeof(*strings)) {
+		return strings[level];
+	}
+	else {
+		return nullptr;
+	}
+}
+
+std::ostream&
+operator<<(std::ostream& os, LogLevel level)
+{
+	auto str = toString(level);
+	if (str) {
+		os << str;
+	}
+	else {
+		os << static_cast<int>(level);
+	}
+
+	return os;
+}
+
 BOOST_LOG_GLOBAL_LOGGER_INIT(Logger, LoggerType)
 {
 	LoggerType lg;
@@ -20,24 +55,32 @@ BOOST_LOG_GLOBAL_LOGGER_INIT(Logger, LoggerType)
 void
 initLogging()
 {
+	namespace keywords = boost::log::keywords;
+	namespace expr = boost::log::expressions;
+	namespace attr = boost::log::attributes;
+
+	auto core = boost::log::core::get();
 #ifdef NDEBUG
-	boost::log::core::get()->set_filter(
-		boost::log::trivial::severity >= LogLevel::info);
+	core->set_filter(severity > LogLevel::debug);
 #endif
 
 	boost::log::add_common_attributes();
-
-	namespace keywords = boost::log::keywords;
-	namespace expr = boost::log::expressions;
+	core->add_global_attribute("Scope", attr::named_scope());
 
 	boost::log::add_console_log(std::cout, keywords::format = "%Message%")
-		->set_filter(boost::log::trivial::severity < LogLevel::error);
-	boost::log::add_console_log(
-		std::cerr, keywords::format = "[%Severity%] %Message%")
-		->set_filter(boost::log::trivial::severity >= LogLevel::error);
+		->set_filter(LogLevel::info <= severity && severity < LogLevel::error);
+	boost::log::add_console_log(std::cerr,
+		keywords::format = expr::stream << "[" << severity << "] "
+										<< expr::message)
+		->set_filter(severity >= LogLevel::error);
 
 	boost::log::add_file_log(keywords::file_name = "xolotlOutput.log",
-		keywords::format = "(%TimeStamp%)[%Severity%]: %Message%");
+		keywords::format = expr::stream
+			<< "("
+			<< expr::format_date_time<boost::posix_time::ptime>(
+				   "TimeStamp", "%Y-%m-%d %H:%M:%S")
+			<< ")[" << severity << "] " << expr::message
+			<< expr::format_named_scope("Scope", keywords::format = "{%n}"));
 }
 
 void
