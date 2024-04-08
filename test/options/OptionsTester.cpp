e00931359872dcbc41b96dd1e563e2a45190b81b
Philip Fackler <facklerpw@ornl.gov>
2024-04-08 11:59:48 -0400

    Removed config format param files

--- test/options/OptionsTester.cpp
+++ test/options/OptionsTester.cpp
@@ -37,39 +37,4 @@ BOOST_AUTO_TEST_CASE(createOptions)
 	BOOST_REQUIRE(dynamic_cast<ConfOptions*>(opts.get()));
 }
 
-BOOST_AUTO_TEST_CASE(compareOptions)
-{
-	auto cases = std::vector<std::string>{"benchmark_AZr_1", "benchmark_NE_1",
-		"benchmark_NE_2", "benchmark_NE_3", "benchmark_NE_4", "benchmark_NE_5",
-		"benchmark_PSI_10", "benchmark_PSI_1", "benchmark_PSI_2",
-		"benchmark_PSI_3", "benchmark_PSI_4", "benchmark_PSI_5",
-		"benchmark_PSI_7", "benchmark_PSI_8", "benchmark_PSI_9", "multi_0D",
-		"multi_1D", "multi", "single_1D", "system_Alloy_1", "system_AZr_0",
-		"system_Fe_1", "system_NE_0", "system_NE_1", "system_NE_2",
-		"system_NE_3", "system_NE_4", "system_PSI_1", "system_PSI_2",
-		"system_PSI_3", "system_PSI_4", "system_PSI_5"};
-
-	::xolotl::fs::current_path(dataDir);
-
-	auto readAndPrint = [](std::shared_ptr<IOptions> opts,
-							const std::string& fileName) {
-		const char* argv[] = {"", fileName.c_str()};
-		opts->readParams(2, argv);
-		std::ostringstream oss;
-		opts->printAll(oss);
-		return oss.str();
-	};
-
-	for (auto&& caseName : cases) {
-		auto fileBaseName = "params_" + caseName;
-
-		auto confStr = readAndPrint(
-			std::make_shared<ConfOptions>(), fileBaseName + ".txt");
-		auto jsonStr = readAndPrint(
-			std::make_shared<JSONOptions>(), fileBaseName + ".json");
-
-		BOOST_REQUIRE(confStr == jsonStr);
-	}
-}
-
 BOOST_AUTO_TEST_SUITE_END()
