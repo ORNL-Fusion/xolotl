df91c58ea155f11551be6baabfa5a1f1f163d595
Philip Fackler <facklerpw@ornl.gov>
2024-09-24 10:27:08 -0500

    Updated usage for Kokkos::Array (#194)

--- xolotl/core/include/xolotl/core/Types.h
+++ xolotl/core/include/xolotl/core/Types.h
@@ -1,3 +1,7 @@
+#pragma once
+
+#include <type_traits>
+
 #include <Kokkos_Core.hpp>
 #include <Kokkos_OffsetView.hpp>
 
@@ -10,8 +14,97 @@ namespace core
 {
 using RowColPair = util::Array<IdType, 2>;
 
-using StencilConcArray = Kokkos::Array<Kokkos::View<const double*>,
-	KOKKOS_INVALID_INDEX, Kokkos::Array<>::contiguous>;
+template <typename T>
+class SizedArrayWrapper
+{
+public:
+	// using reference = T&;
+	// using const_reference = std::add_const_t<T>&;
+	// using size_type = size_t;
+	// using difference_type = ptrdiff_t;
+	// using value_type = T;
+	// using pointer = T*;
+	// using const_pointer = std::add_const_t<T>*;
+
+	KOKKOS_INLINE_FUNCTION
+	SizedArrayWrapper(T* data, std::size_t size) : _data(data), _size(size)
+	{
+	}
+
+	KOKKOS_INLINE_FUNCTION
+	SizedArrayWrapper&
+	operator=(const SizedArrayWrapper<T>& rhs)
+	{
+		const size_t n = size() < rhs.size() ? size() : rhs.size();
+		for (size_t i = 0; i < n; ++i) {
+			_data[i] = rhs[i];
+		}
+		return *this;
+	}
+
+	template <typename TIndex>
+	KOKKOS_INLINE_FUNCTION
+	T&
+	operator[](const TIndex& i)
+	{
+		static_assert((std::is_integral_v<TIndex> || std::is_enum_v<TIndex>),
+			"Must be integral argument");
+		assert(i < _size);
+		return _data[i];
+	}
+
+	template <typename TIndex>
+	KOKKOS_INLINE_FUNCTION
+	const T&
+	operator[](const TIndex& i) const
+	{
+		static_assert((std::is_integral_v<TIndex> || std::is_enum_v<TIndex>),
+			"Must be integral argument");
+		assert(i < _size);
+		return _data[i];
+	}
+
+	KOKKOS_INLINE_FUNCTION
+	std::size_t
+	size() const
+	{
+		return _size;
+	}
+
+	KOKKOS_INLINE_FUNCTION
+	bool
+	empty() const
+	{
+		return 0 == _size;
+	}
+
+	KOKKOS_INLINE_FUNCTION
+	std::size_t
+	max_size() const
+	{
+		return _size;
+	}
+
+	KOKKOS_INLINE_FUNCTION
+	T*
+	data()
+	{
+		return _data;
+	}
+
+	KOKKOS_INLINE_FUNCTION
+	const T*
+	data() const
+	{
+		return _data;
+	}
+
+private:
+	T* _data;
+	std::size_t _size;
+};
+
+using StencilConcArray = SizedArrayWrapper<Kokkos::View<const double*>>;
 } // namespace core
 
 using DefaultMemSpace = Kokkos::DefaultExecutionSpace::memory_space;
