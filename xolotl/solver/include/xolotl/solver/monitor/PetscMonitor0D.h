be5fc0ce46bf49f918144d5bdef8a390f07db9f7
Adithya Nair <adithya.nair.a@gmail.com>
2026-04-30 10:08:39 -0400

    Add declarations for network-specific bubble monitor functions

--- xolotl/solver/include/xolotl/solver/monitor/PetscMonitor0D.h
+++ xolotl/solver/include/xolotl/solver/monitor/PetscMonitor0D.h
@@ -57,8 +57,14 @@ public:
 	computeAlphaZr(
 		TS ts, PetscInt timestep, PetscReal time, Vec solution) override;
 
-	PetscErrorCode
-	monitorBubble(TS ts, PetscInt timestep, PetscReal time, Vec solution);
+//	PetscErrorCode
+//	monitorBubble(TS ts, PetscInt timestep, PetscReal time, Vec solution);
+
+	PetscErrorCode monitorBubbleV(
+        TS ts, PetscInt timestep, PetscReal time, Vec solution);
+
+    	PetscErrorCode monitorBubbleFe(
+        TS ts, PetscInt timestep, PetscReal time, Vec solution);
 
 protected:
 	std::shared_ptr<viz::IPlot> _scatterPlot;
